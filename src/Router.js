666a79bb16c5469ef369002b338b234a9550c211
이누인웅's air <macbookair@inu-in-ung.local>
2021-12-01 17:16:07 +0900

    카트 페이지 생성

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login/Login';
+import Cart from './pages/Cart/Cart';
 function Router() {
   <BrowserRouter>
     <Routes>
-      <Route path="/Login">
-        <Login />
-      </Route>
+      <Route path="/Login" element={<Login />} />
+      <Route path="/Cart" element={<Cart />} />
     </Routes>
   </BrowserRouter>;
 }
---
+++ src/pages/Cart/Cart.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Cart() {
+  return <div>하이</div>;
+}
+
+export default Cart;
