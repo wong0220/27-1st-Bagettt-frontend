fa8b1883449f04b72e416bd000d8582d40434aea
Jaeho <benteke@naver.com>
2021-12-05 14:45:27 +0900

    Add:Nav바 작업중

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -5,27 +5,37 @@ function Nav() {
   return (
     <div>
       <nav className="nav">
-        <div className="navPhoto">
-          <img
-            className="brandPhoto"
-            src="https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMDEyMDJfMTM5%2FMDAxNjA2ODk4OTQ5MDI5.Hz827ierbsGaEg2OTsbeRZyBJk_hhvZnw2ctbEbPWoQg.3pB-sXhL3i6iJDbP1gCAeqp_W2qqrMrN5t9Mdpq75eQg.JPEG.emanaya%2F8447A2E8-D307-432B-8AB9-1BFAAE4B00F9.JPG&type=sc960_832"
-            alt="brandPhoto"
-          />
+        <div className="navHeader">
+          <div className="navPhoto">
+            <img
+              className="brandPhoto"
+              src="image\baGETTT_logo.png"
+              alt="brandPhoto"
+            />
+          </div>
+          <div className="navList">
+            <ul className="list">
+              <li className="brandStory">BaGETTT Story</li>
+              <li className="subScribe">SUBSCRIBE</li>
+              <li className="event">EVENT</li>
+              <li className="brand">BRAND</li>
+              <li className="review">REVIEW</li>
+              <li className="customerCenter">CUSTOMER CENTER</li>
+            </ul>
+          </div>
+          <div className="icons">
+            <i className="fas fa-search fa-2x" />
+            <i className="fas fa-user fa-2x" />
+            <i className="fas fa-shopping-bag fa-2x" />
+          </div>
         </div>
-        <div className="navList">
-          <ul className="list">
-            <li className="brandStory">BaGETTT Story</li>
-            <li className="subScribe">SUBSCRIBE</li>
-            <li className="event">EVENT</li>
-            <li className="brand">BRAND</li>
-            <li className="review">REVIEW</li>
-            <li className="customerCenter">CUSTOMER CENTER</li>
+        <div className="blindList">
+          <ul className="otherList">
+            <li className="history">BaGETTT's History</li>
+            <li className="cheifPick">Cheif's Pick</li>
+            <li className="location">BaGETTT's Location</li>
+            <li className="magaZine">BaGETTT's Magazine</li>
           </ul>
-        </div>
-        <div className="icons">
-          <i className="fas fa-search fa-2x" />
-          <i className="fas fa-user fa-2x" />
-          <i className="fas fa-shopping-bag fa-2x" />
         </div>
       </nav>
     </div>
