c153ed890ace31f772ff5d859a471595cf21869e
Jaeho <benteke@naver.com>
2021-12-08 15:03:09 +0900

    ADD:Nav 바

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,44 +1,53 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Nav.scss';
 
 function Nav() {
   return (
-    <div>
-      <nav className="nav">
-        <div className="navHeader">
-          <div className="navPhoto">
+    <nav className="nav">
+      <div className="navHeader">
+        <div className="navPhoto">
+          <Link to="/main">
             <img
               className="brandPhoto"
               src="image\baGETTT_logo.png"
               alt="brandPhoto"
             />
-          </div>
-          <div className="navList">
-            <ul className="list">
-              <li className="brandStory">BaGETTT Story</li>
-              <li className="subScribe">SUBSCRIBE</li>
-              <li className="event">EVENT</li>
-              <li className="brand">BRAND</li>
-              <li className="review">REVIEW</li>
-              <li className="customerCenter">CUSTOMER CENTER</li>
-            </ul>
-          </div>
-          <div className="icons">
-            <i className="fas fa-search fa-2x" />
-            <i className="fas fa-user fa-2x" />
-            <i className="fas fa-shopping-bag fa-2x" />
-          </div>
+          </Link>
         </div>
-        <div className="blindList">
-          <ul className="otherList">
-            <li className="history">BaGETTT's History</li>
-            <li className="cheifPick">Cheif's Pick</li>
-            <li className="location">BaGETTT's Location</li>
-            <li className="magaZine">BaGETTT's Magazine</li>
-          </ul>
+
+        <ul className="list">
+          <li className="brandStory">
+            <div className="story">BaGETTT Story</div>
+            <div className="subMenu">
+              <div className="subMenuitems">
+                <ul className="otherList">
+                  <li className="history">BaGETTT's History</li>
+                  <li className="cheifPick">Cheif's Pick</li>
+                  <li className="location">BaGETTT's Location</li>
+                  <li className="magaZine">BaGETTT's Magazine</li>
+                </ul>
+              </div>
+            </div>
+          </li>
+          <Link to="/list-page">
+            <li className="subScribe">SUBSCRIBE</li>
+          </Link>
+          <li className="event">EVENT</li>
+          <li className="brand">BRAND</li>
+          <li className="review">REVIEW</li>
+          <li className="customerCenter">CUSTOMER CENTER</li>
+        </ul>
+        <div className="blank" />
+        <div className="icons">
+          <i className="fas fa-search fa-lg" />
+          <i className="fas fa-user fa-lg" />
+          <Link to="/Cart">
+            <i className="fas fa-shopping-bag fa-lg" />
+          </Link>
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 }
 
